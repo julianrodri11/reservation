375d48b353b7dc8c56770fb36ae4721019a2b95a
Julian Rodriguez <julianrodri11@gmail.com>
2024-11-17 13:28:05 -0500

    Jwt a variable de entorno

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,16 +1,18 @@
 package utils
 
 import (
-    "github.com/golang-jwt/jwt/v4"
-    "time"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
-var jwtKey = []byte("secret_key")
+var jwtKeySecret = []byte(os.Getenv("JWT_SECRET"))
 
 func GenerateJWT(userID uint) (string, error) {
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-        "user_id": userID,
-        "exp":     time.Now().Add(time.Hour * 24).Unix(),
-    })
-    return token.SignedString(jwtKey)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	})
+	return token.SignedString(jwtKeySecret)
 }
